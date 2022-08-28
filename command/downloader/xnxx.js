1684522c383647ad7a86dbe6006d0376afe090f1
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-28 10:24:31 +0700

    Update xnxx.js

--- command/downloader/xnxx.js
+++ command/downloader/xnxx.js
@@ -1,12 +1,12 @@
-const { fetchUrl, isUrl } = require("../../lib/function")
+const { fetchUrl, isUrl } = require("../../lib/Function")
 
 module.exports = {
     name: "xnxx",
     alias: ["xnxx"],
     use: "<url>",
     desc: "Download Media From https://xnxx.com",
-    type: "downloader",
-    example: "%prefix%command <url>",
+    category: "downloader",
+    query: "%prefix%command <url>",
     start: async(killua, m, { prefix, command, text }) => {
         let fetch = await fetchUrl(global.api("zenz", "/downloader/xnxx", { url: isUrl(text)[0] }, "apikey"))
         let teks = `⭔ Title : ${fetch.result.title}\n⭔ Duration : ${fetch.result.duration}s`
