13e36b37211f47bbb48b0aaf00000448faf92c4f
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-28 10:32:51 +0700

    Update xnxx.js

--- command/downloader/xnxx.js
+++ command/downloader/xnxx.js
@@ -1,4 +1,4 @@
-const { fetchUrl, isUrl } = require("../../lib/Function")
+const { fetchUrl, isUrl } = require("../../lib/function")
 
 module.exports = {
     name: "xnxx",
@@ -7,7 +7,7 @@ module.exports = {
     desc: "Download Media From https://xnxx.com",
     category: "downloader",
     query: "%prefix%command <url>",
-    start: async(killua, m, { prefix, command, text }) => {
+    async(killua, m, { prefix, command, text }) => {
         let fetch = await fetchUrl(global.api("zenz", "/downloader/xnxx", { url: isUrl(text)[0] }, "apikey"))
         let teks = `⭔ Title : ${fetch.result.title}\n⭔ Duration : ${fetch.result.duration}s`
         killua.sendFile(m.from, fetch.result.files.low, "", m, { caption: teks })
