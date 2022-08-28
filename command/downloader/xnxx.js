dc10e59996cdf8af6adadc9383aef77adfbc33f6
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-28 12:08:36 +0700

    Update xnxx.js

--- command/downloader/xnxx.js
+++ command/downloader/xnxx.js
@@ -1,19 +1,18 @@
+let { fetchUrl, isUrl } = require('../../lib/function')
+
 module.exports = {
-  name: "anime",
-  alias: ["anime"],
+  name: "xnxxdl",
+  alias: ["xnxxdl"],
   category: "downloader",
   isOwner: false,
   isGroup: false,
   isBotAdmin: false,
 	isAdmin: false,
-  async run({msg, conn},{map, args, q}){
-    const { sender, from, command } = msg
-    switch(command){
-      
-      case 'anime': case 'waifu': case 'husbu': case 'neko': case 'shinobu': case 'megumin': case 'waifus': case 'nekos': case 'trap': case 'blowjob': {
-                m.reply(mess.wait)
-                xavior.sendMessage(m.chat, { image: { url: api('zenz', '/api/random/'+command, {}, 'apikey') }, caption: 'Generate Random ' + command }, { quoted: m })
-            }
+  async(killua, m, { prefix, command, text }) => {
+        let fetch = await fetchUrl(global.api("zenz", "/downloader/xnxx", { url: isUrl(text)[0] }, "apikey"))
+        let teks = `⭔ Title : ${fetch.result.title}\n⭔ Duration : ${fetch.result.duration}s`
+        killua.sendFile(m.from, fetch.result.files.low, "", m, { caption: teks })
+    }
           
         } catch (e){
 	    global.error(msg.command, e, msg)
