21542d53f195db19ce03f02d0028a05a66ed38d9
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-08-28 14:31:56 +0700

    Update xnxx.js

--- command/downloader/xnxx.js
+++ command/downloader/xnxx.js
@@ -1,4 +1,4 @@
-let { fetchUrl, isUrl } = require('../../lib/function')
+let { monospace, isUrl } = require('../../lib/function')
 
 module.exports = {
   name: "xnxxdl",
@@ -11,13 +11,12 @@ module.exports = {
   async run({msg, conn},{map, args, q}){
 const { sender, from, command } = msg
 switch(command){
-if(!q) throw "Not query Url"
+if(!q) throw "𝙼𝚊𝚜𝚞𝚔𝚊𝚗 𝚕𝚒𝚗𝚔"
 	  try {
         let fetch = await fetchUrl(global.api("zenz", "/downloader/xnxx", { url: isUrl(text)[0] }, "apikey"))
         let teks = `⭔ Title : ${fetch.result.title}\n⭔ Duration : ${fetch.result.duration}s`
         conn.sendFile(m.from, fetch.result.files.low, "", m, { caption: teks })
-    }
-          
+    }          
         } catch (e){
 	    global.error(msg.command, e, msg)
 	  }
